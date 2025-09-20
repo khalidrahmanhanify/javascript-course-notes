e0dc3754b62125ea118f36e0a2891f3cf9415c89
khalidrahmanhanify <khalidrahmanhanify@gmail.com>
2025-09-20 16:00:54 +0430

    Fixed link issues

--- assets/js/generateBreadcrumb.js
+++ assets/js/generateBreadcrumb.js
@@ -2,6 +2,7 @@ function generateBreadCrumb() {
   const breadCrumbContainer = document.getElementById("breadcrumb");
   let pathArray = window.location.pathname.split("/").filter((p) => p);
   const repoName = "javascript-course-notes";
+  pathArray.unshift(repoName);
 
   // If last segment is index.html, drop it
   if (pathArray[pathArray.length - 1]?.toLowerCase() === "index.html") {
@@ -9,22 +10,22 @@ function generateBreadCrumb() {
   }
 
   let breadcrumbHTML = `
-    <a href="/index.html">
-      <img src="${repoName}/assets/Icons/monitor.png" style="width: 30px;"/>
+    <a href="https://khalidrahmanhanify.github.io/${repoName}/index.html">
+      <img src="https://khalidrahmanhanify.github.io/${repoName}/assets/Icons/monitor.png" style="width: 30px;"/>
     </a>
-     <img src="${repoName}/assets/Icons/arrow_right.png" style="width: 20px; height: 20px;"/>
+     <img src="https://khalidrahmanhanify.github.io/${repoName}/assets/Icons/arrow_right.png" style="width: 20px; height: 20px;"/>
   `;
 
   let fullPath = "";
-  pathArray.forEach((segment, index) => {
-    if (segment === repoName) return;
+  pathArray.forEach((segment) => {
     fullPath += `/${segment}`;
+    if (segment === repoName) return;
 
     breadcrumbHTML += `
      
     `;
     breadcrumbHTML += `<a href="${fullPath}">${decodeURIComponent(segment)}</a>
-       <img src="${repoName}/assets/Icons/arrow_right.png" style="width: 20px; height: 20px;"/>`;
+       <img src="https://khalidrahmanhanify.github.io/${repoName}/assets/Icons/arrow_right.png" style="width: 20px; height: 20px;"/>`;
   });
   console.log(fullPath);
 
